46830103d4dee180939d5395722e2f03e9bb30d7
Name <reva00@ngs.ru>
2019-11-25 13:13:50 +0700

    version1

--- life.py
+++ life.py
@@ -1,4 +1,25 @@
 import numpy as np
 
 def step(state):
-    pass
+    shape = np.shape(state)
+    new_state =np.zeros((shape[0]+2,shape[1]+2))
+    new_state[:-2,:-2] += state
+    new_state[1:-1,:-2] += state
+    new_state[2:,:-2] += state
+    new_state[2:,1:-1] += state
+    new_state[2:,2:] += state
+    new_state[1:-1,2:] += state
+    new_state[:-2,2:] += state
+    new_state[:-2,1:-1:] += state
+    new_state = new_state[1:-1,1:-1]
+    new_state = state*(new_state-0)*(new_state-1)*(new_state-4)*(new_state-5)*(new_state-6)*(new_state-7)*(new_state-8)+(new_state-0)*(new_state-1)*(new_state-2)*(new_state-4)*(new_state-5)*(new_state-6)*(new_state-7)*(new_state-8)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            new_state[i,j] = bool(new_state[i,j])
+    return new_state
+    
+
+'''a = np.array([[False,True,False],[False,True,False],[False,True,False],[False,False,False]])
+print(step(a))
+print(step(step(a)))
+print(step(step(step(a))))'''
