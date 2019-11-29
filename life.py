e9d922ee05777988fbea8f0278bce1814c9bd8a7
Nikolay Davydov <davnvl2000@gmail.com>
2019-11-30 01:31:04 +0700

    Implementation compleat

--- life.py
+++ life.py
@@ -1,4 +1,18 @@
 import numpy as np
 
 def step(state):
-    pass
+    width = state[0].size
+    hight = state.size // width
+
+    next_step = np.array(state)
+
+    for row_ind in np.arange(hight):
+    	for col_ind in np.arange(width):
+    		statement = state[row_ind][col_ind]
+    		neighbours = sum([state[i % hight][j % width] for i in range(row_ind - 1, row_ind + 2) for j in range(col_ind - 1, col_ind + 2)]) - statement
+    		if(neighbours == 3):
+    			next_step[row_ind][col_ind] = True
+    		elif(neighbours != 2 and neighbours != 3):
+    			next_step[row_ind][col_ind] = False
+
+    return next_step
