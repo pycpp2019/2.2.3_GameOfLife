89af17438f25b1ea2d995a83e7736e3853048930
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-12-02 10:11:55 +0700

    reference solution

--- life.py
+++ life.py
@@ -1,4 +1,14 @@
 import numpy as np
 
 def step(state):
-    pass
+    adj = np.zeros(state.shape)
+    for j in range(-1, 2):
+        for i in range(-1, 2):
+            if i != 0 or j != 0:
+                adj += np.roll(state, (j, i), axis=(1, 0))
+    eq2 = adj == 2
+    eq3 = adj == 3
+    return np.logical_or(
+        np.logical_and(state, np.logical_or(eq2, eq3)), # not die
+        np.logical_and(np.logical_not(state), eq3) # born
+    )
