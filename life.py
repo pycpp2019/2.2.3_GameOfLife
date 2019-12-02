dde74711296161c1c1a1e8d70ab0b8a134756e14
Nikolay Davydov <davnvl2000@gmail.com>
2019-12-02 21:57:23 +0700

    Implemented using numpy.roll

--- life.py
+++ life.py
@@ -1,18 +1,16 @@
 import numpy as np
 
 def step(state):
-    width = state[0].size
-    hight = state.size // width
+    
+    state_int = state.astype(int)
+    tmp_state = (np.roll(state_int, 1, axis = 0)
+        + np.roll(np.roll(state_int, 1, axis = 0), 1, axis = 1)
+        + np.roll(np.roll(state, 1, axis = 0), -1, axis = 1)
+        + np.roll(state, -1, axis = 0)
+        + np.roll(np.roll(state, -1, axis = 0), 1, axis = 1)
+        + np.roll(np.roll(state, -1, axis = 0), -1, axis = 1)
+        + np.roll(state, 1, axis = 1)
+        + np.roll(state, -1, axis = 1)
+        )
 
-    next_step = np.array(state)
-
-    for row_ind in np.arange(hight):
-    	for col_ind in np.arange(width):
-    		statement = state[row_ind][col_ind]
-    		neighbours = sum([state[i % hight][j % width] for i in range(row_ind - 1, row_ind + 2) for j in range(col_ind - 1, col_ind + 2)]) - statement
-    		if(neighbours == 3):
-    			next_step[row_ind][col_ind] = True
-    		elif(neighbours != 2 and neighbours != 3):
-    			next_step[row_ind][col_ind] = False
-
-    return next_step
+    return state * np.logical_or(tmp_state == 2, tmp_state == 3) + (tmp_state == 3)
