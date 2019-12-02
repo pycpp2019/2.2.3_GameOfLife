0070efeec59465f4229d07031344224d89f45aaa
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-12-02 09:43:59 +0700

    test oscillator step and  timeout

--- test_life.py
+++ test_life.py
@@ -1,6 +1,29 @@
+import pytest
+
 import numpy as np
 
 from life import step
+
+
+def test_oscillator():
+    data = np.array([
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,1,1,1,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+    ], dtype=np.bool)
+
+    new_data = step(data)
+    ref_data = np.array([
+        [0,0,0,0,0],
+        [0,0,1,0,0],
+        [0,0,1,0,0],
+        [0,0,1,0,0],
+        [0,0,0,0,0],
+    ], dtype=np.bool)
+
+    assert np.all(new_data == ref_data)
 
 def test_glider():
     data = np.array([
@@ -12,7 +35,25 @@ def test_glider():
     ], dtype=np.bool)
 
     new_data = data
-    for _ in range(2*10):
+    for _ in range(20):
+        new_data = step(new_data)
+
+    assert np.all(data == new_data)
+
+@pytest.mark.timeout(60)
+def test_glider_large_field():
+    size = 500
+    data = np.zeros((size, size), dtype=np.bool)
+    data[:5,:5] = np.array([
+        [0,0,0,0,0],
+        [0,0,1,0,0],
+        [0,0,0,1,0],
+        [0,1,1,1,0],
+        [0,0,0,0,0],
+    ], dtype=np.bool)
+
+    new_data = data
+    for _ in range(4*size):
         new_data = step(new_data)
 
     assert np.all(data == new_data)
