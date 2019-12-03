656873e2836d7be4671db7cd2024734f255dcb62
AlexBorodulina <55827253+AlexBorodulina@users.noreply.github.com>
2019-12-03 17:57:34 +0700

    Add files via upload

--- life.py
+++ life.py
@@ -1,22 +1,8 @@
 import numpy as np
-	
-def TwoAndThree(a):
-	if a==2 or a==3:
-		return 1
-	else:
-		return 0
-
-def Three(a):
-	if a==3:
-		return 1
-	else:
-		return 0
-
 def step(state):
 	state=np.array(state)
-	stateSum=np.roll(state,1,axis=0)+np.roll(state,-1,axis=0)+np.roll(state,1,axis=1)+np.roll(state,-1,axis=1)+np.roll(np.roll(state,1,axis=0),1,axis=1)+np.roll(np.roll(state,1,axis=0),-1,axis=1)+np.roll(np.roll(state,-1,axis=0),1,axis=1)+np.roll(np.roll(state,-1,axis=0),-1,axis=1)
-	TwoAndThreeV=np.vectorize(TwoAndThree)
-	ThreeV=np.vectorize(Three)
-	StateSumTT=TwoAndThreeV(stateSum)*state
-	StateSumT=np.logical_xor(ThreeV(stateSum),state)*ThreeV(stateSum)
-	return np.array(StateSumT+StateSumTT,dtype=bool)
+	StateSum=np.roll(state,1,axis=0)+np.roll(state,-1,axis=0)+np.roll(state,1,axis=1)+np.roll(state,-1,axis=1)+np.roll(np.roll(state,1,axis=0),1,axis=1)+np.roll(np.roll(state,1,axis=0),-1,axis=1)+np.roll(np.roll(state,-1,axis=0),1,axis=1)+np.roll(np.roll(state,-1,axis=0),-1,axis=1)
+	StateSum2 = StateSum == 2
+	StateSum3 = StateSum == 3
+	StateSum = np.logical_or(np.logical_and(np.logical_or(StateSum2 , StateSum3),state),StateSum3)
+	return StateSum
