9b721259d1238472f5e048ff7848d8473b20861e
AlexBorodulina <55827253+AlexBorodulina@users.noreply.github.com>
2019-12-03 18:36:07 +0700

    Add files via upload

--- life.py
+++ life.py
@@ -1,5 +1,5 @@
 import numpy as np
 def step(state):
-	state=np.array(state,dtype=int)
-	StateSum=np.roll(state,1,axis=0)+np.roll(state,-1,axis=0)+np.roll(state,1,axis=1)+np.roll(state,-1,axis=1)+np.roll(np.roll(state,1,axis=0),1,axis=1)+np.roll(np.roll(state,1,axis=0),-1,axis=1)+np.roll(np.roll(state,-1,axis=0),1,axis=1)+np.roll(np.roll(state,-1,axis=0),-1,axis=1)
+	state1=np.array(state,dtype=int)
+	StateSum=np.roll(state1,1,axis=0)+np.roll(state1,-1,axis=0)+np.roll(state1,1,axis=1)+np.roll(state1,-1,axis=1)+np.roll(np.roll(state1,1,axis=0),1,axis=1)+np.roll(np.roll(state1,1,axis=0),-1,axis=1)+np.roll(np.roll(state1,-1,axis=0),1,axis=1)+np.roll(np.roll(state1,-1,axis=0),-1,axis=1)
 	return state*np.logical_or(StateSum == 2, StateSum == 3)+(StateSum == 3)
