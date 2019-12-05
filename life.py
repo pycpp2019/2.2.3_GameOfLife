b202765ae05d9080f228fc0993c1e83f018e415a
obelova <you@example.com>
2019-12-05 21:09:45 +0700

    Solution

--- life.py
+++ life.py
@@ -1,4 +1,8 @@
 import numpy as np
 
 def step(state):
-    pass
+    new_state = -np.array(state, dtype=int)
+    for i in [-1,0,1]:
+        for j in [-1,0,1]:
+            new_state += np.roll(np.roll(state, i, axis=0), j, axis=1)
+    return np.logical_or(new_state==3, np.logical_and(new_state==2, state==True))
