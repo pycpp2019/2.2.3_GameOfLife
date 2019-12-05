bbb95ed4bb418b7348c81c04c7ad8ffc32112610
Dmitriev Maxim <example.email@example>
2019-12-05 15:24:22 +0700

    Conway's_game_of_live

--- life.py
+++ life.py
@@ -1,4 +1,18 @@
 import numpy as np
 
 def step(state):
-    pass
+    return  (to_neighbors(state)==3)  +  ( state * (to_neighbors(state)==2)  ) 
+
+def ver():
+    return 2
+        
+def to_neighbors(state):
+    st=1*state
+    return (np.roll(st, 1, axis = 0) + 
+            np.roll(st,-1, axis = 0) +
+            np.roll(st, 1, axis = 1) +
+            np.roll(st,-1, axis = 1) +
+            np.roll(np.roll(st, 1, axis = 1), 1, axis = 0) +
+            np.roll(np.roll(st,-1, axis = 1), 1, axis = 0) +
+            np.roll(np.roll(st, 1, axis = 1),-1, axis = 0) +
+            np.roll(np.roll(st,-1, axis = 1),-1, axis = 0))
