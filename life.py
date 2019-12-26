38814edcda1877876cd48a370a8c6b1d46ff2d39
Solovev-Sergey <55825616+Solovev-Sergey@users.noreply.github.com>
2019-12-26 14:01:48 +0700

    Update life.py

--- life.py
+++ life.py
@@ -1,4 +1,18 @@
 import numpy as np
 
 def step(state):
-    pass
+    return  (neighbors(state)==3)  +  ( state * (neighbors(state)==2)  ) 
+
+def ver():
+    return 2
+        
+def neighbors(state):
+    st=1*state
+    return (np.roll(st, 1, axis = 0) + 
+            np.roll(st,-1, axis = 0) +
+            np.roll(st, 1, axis = 1) +
+            np.roll(st,-1, axis = 1) +
+            np.roll(np.roll(st, 1, axis = 1), 1, axis = 0) +
+            np.roll(np.roll(st,-1, axis = 1), 1, axis = 0) +
+            np.roll(np.roll(st, 1, axis = 1),-1, axis = 0) +
+            np.roll(np.roll(st,-1, axis = 1),-1, axis = 0))
